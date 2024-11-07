8a1ecc0383c4992f3bb897db41a970ae4d677751
Lucky357231 <mojnovitie@bk.ru>
2024-11-08 01:35:53 +0300

    Update Unit-test

--- shop/tests/test_views.py
+++ shop/tests/test_views.py
@@ -1,26 +1,60 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
-from shop.models import Product, Purchase
+from shop.models import Product, Purchase, Customer
 
-class PurchaseCreateTestCase(TestCase):
+
+class IndexViewTests(TestCase):
     def setUp(self):
-        self.client = Client()
-        self.product = Product.objects.create(name="book", price=740)
+        self.product = Product.objects.create(name="Тестовый продукт", price=1000)
 
-    def test_webpage_accessibility(self):
+    def test_index_view(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/index.html')
+        self.assertIn('message', response.context)
+        self.assertEqual(response.context['message'], "Просмотрите наши товары и накопительную систему скидок!")
+        self.assertIn('products', response.context)
+        self.assertEqual(list(response.context['products']), [self.product])
 
-    def test_create_purchase_with_discount(self):
-        response = self.client.post(reverse('buy', args=[self.product.id]), {
-            'person': 'Ivanov',
-            'email': 'ivanov@example.com',
-            'address': 'Svetlaya St.'
-        })
+
+class PurchaseCreateViewTests(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name="Тестовый продукт", price=1000)
+        self.customer = Customer.objects.create(name="Иван Иванов", total_purchases=5)
+
+    def test_purchase_create_view_get(self):
+        response = self.client.get(reverse('buy', kwargs={'product_id': self.product.id}))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Спасибо за покупку, Ivanov!', response.content)
+        self.assertTemplateUsed(response, 'shop/purchase_form.html')
+        self.assertEqual(response.context['product'], self.product)
 
-        # Проверяем, что покупка создалась в базе данных
-        purchase = Purchase.objects.get(customer_name='Ivanov')
+    def test_purchase_create_view_post(self):
+        data = {
+            'customer_name': self.customer.name,
+            'address': '123 Test Street'
+        }
+        response = self.client.post(reverse('buy', kwargs={'product_id': self.product.id}), data=data)
+
+        self.customer.refresh_from_db()
+        self.assertEqual(self.customer.total_purchases, 6)
+
+        # Логика расчета скидки
+        expected_discount = 15 if self.customer.total_purchases > 10 else 0
+        expected_discounted_price = int(self.product.price * (1 - expected_discount / 100))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/receipt.html')
+        self.assertEqual(response.context['customer'], self.customer)
+        self.assertEqual(response.context['product'], self.product)
+        self.assertEqual(response.context['total_purchases'], self.customer.total_purchases)
+        self.assertEqual(response.context['discount'], expected_discount)
+        self.assertEqual(response.context['original_price'], self.product.price)
+        self.assertEqual(response.context['discounted_price'], expected_discounted_price)
+        self.assertIsNotNone(response.context['purchase_date'])
+
+        # Проверка значения discounted_price
+        purchase = Purchase.objects.latest('date')
         self.assertEqual(purchase.product, self.product)
-        self.assertEqual(purchase.customer_email, 'ivanov@example.com')
+        self.assertEqual(purchase.customer, self.customer)
+        self.assertEqual(purchase.address, data['address'])
+
